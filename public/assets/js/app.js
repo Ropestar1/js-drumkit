b125a5a74ea76a9a1265e75e6a8632237a30baf0
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-09-06 00:44:34 -0700

    Adding more js functionality in ES6 syntax.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,9 @@
 window.addEventListener('keydown', function(e) {
-  console.log(e);
+  // console.log(e);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //this line uses an attribute selector
+  /*
+  if above line were to be written in previous js syntax, I think we need to create a variable,
+  concatenate a string for the attribute selector (audio[data-key='"' + variable for e.keyCode + '""'])
+  */
+  console.log(audio);
 });
