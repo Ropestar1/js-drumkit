4bd3b30f471b5884036a7aed07cd849889f30e19
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-09-08 22:16:58 -0700

    Finishing up functionality.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-window.addEventListener('keydown', function(e) {
-  // console.log(e);
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //this line uses an attribute selector
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   /*
   if above line were to be written in previous js syntax, I think we need to create a variable,
   concatenate a string for the attribute selector (audio[data-key='"' + variable for e.keyCode + '""'])
@@ -10,5 +10,16 @@ window.addEventListener('keydown', function(e) {
 
   audio.currentTime = 0; // sets the play time of the audio to 0, starting over
   audio.play()
-  console.log(audio);
-});
+
+  key.classList.add('playing'); //also can use toggle instead of add or remove
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
